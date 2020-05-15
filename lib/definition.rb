9dc805034fd68f44f567ded8f42cfd3716619867
Reid Ashwill <reidashwill@gmail.com>
2020-05-15 09:40:38 -0700

    after some serious head scratching about why my first test would not pass, i realized that i was not requiring word in my tests.  first definition class test passing sucesfully.  Also running into some strange coloration in VSC that i have not seen before.  Might be because definition is a key word elsewhere?  I will look into this further on my own time.

--- lib/definition.rb
+++ lib/definition.rb
@@ -1,18 +1,19 @@
  class Definition
+
   attr_reader :id
-  attr_accessor :def, :word_id
-  
+  attr_accessor :name, :word_id
+
   @@definitions = {}
   @@total_rows = 0
 
   def initialize(attributes)
-    @def = attributes.fetch(:def)
-    @id = attributes.fetch(:id) || @@total_rows +=1
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
     @word_id = attributes.fetch(:word_id)
-  end
+  end 
 
   def == (definition_to_compare)
-    (self.def() == definition_to_compare.def()) && (self.word_id() == definition_to_compare.word_id())
+    (self.name() == definition_to_compare.name()) && (self.word_id() == definition_to_compare.word_id())
   end
 
   def self.all
@@ -28,8 +29,8 @@
     @@definitions[id]
   end
 
-  def save
-    @@definitions[self.id] = Definition.new({ :def => self.def, :id => self.id, :word_id => self.word_id})
-  end
+  # def save
+  #   @@definitions[self.id] = Definition.new({ :def => self.def, :id => self.id, :word_id => self.word_id})
+  # end
 
 end  
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -8,10 +8,17 @@ describe '#Definition' do
   before(:each) do
     Definition.clear()
     Word.clear()
-    word = Word.new({:name => "Enterprise", :id => nil})
-    word.save()
+    @word = Word.new({:name => "Enterprise", :id => nil})
+    @word.save()
   end
 
+  describe('#==') do
+    it ('is the same definition if it has the same attributes as anothe definition') do
+      definition1 = Definition.new({:name => "The USS Enterprise in a galaxy class starship of the United Federation of Planets.  It has a registered as NCC-1701-D", :id => nil, :word_id => @word.id})
+      definition2 = Definition.new({:name => "The USS Enterprise in a galaxy class starship of the United Federation of Planets.  It has a registered as NCC-1701-D", :id => nil, :word_id => @word.id})
+      expect(definition1).to(eq(definition2))
+    end
+  end
 
 
 end
