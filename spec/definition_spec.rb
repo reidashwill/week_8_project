56a45537c5acd075ca0892843e79d1db7f40c774
Reid Ashwill <reidashwill@gmail.com>
2020-05-15 10:07:23 -0700

    add failing tests for the unwritten methods .find_by_word and #word

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -81,4 +81,22 @@ describe '#Definition' do
       expect(Definition.all).to(eq([definition2]))
     end
   end
+
+  describe('.find_by_word') do
+    it('finds all definitions inside a word based on word_id') do
+      definition1 = Definition.new({:name => "The USS Enterprise in a galaxy class starship of the United Federation of Planets.  It has a registered as NCC-1701-D", :id => nil, :word_id => @word.id})
+      definition1.save()
+      definition2 = Definition.new({:name => "The starship commanded by the bald guy in that Trek Wars show", :id => nil, :word_id => @word.id})
+      definition2.save()
+      expect(Definition.find_by_word(@word.id)).to(eq([definition1, definition2]))
+    end
+  end
+
+  describe('word') do
+    it('finds the word a definition belongs to') do
+      definition1 = Definition.new({:name => "The USS Enterprise in a galaxy class starship of the United Federation of Planets.  It has a registered as NCC-1701-D", :id => nil, :word_id => @word.id})
+      definition1.save()
+      expect(difinition1.word()).to(eq(@word))
+    end
+  end
 end
