09ea2cbd40e9eef5d58521d775d4a1cafea0fc8b
Reid Ashwill <reidashwill@gmail.com>
2020-05-15 08:16:55 -0700

    add basic methods to word class.  testing to follow

---
+++ lib/word.rb
@@ -0,0 +1,23 @@
+class Word
+
+  attr_reader :id, :name
+
+  @@total_rows = 0
+  @@words = {}
+
+  def initialize (attributes)
+    @name = attributes.fetch(:name)
+    @id = attributes.fetch(:id) || @@total_rows += 1
+  end
+
+  def self.all
+    @@words.values()
+  end
+
+  def save
+    @@words[self.id] = Word.new({:name => self.name, :id => self.id})
+  end
+
+  def ==(word_to_compare)
+    self.name() == word_to_compare.name()
+  end  
