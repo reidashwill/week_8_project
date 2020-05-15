19121f9a1496a576a4c03b49d5dc66a269a60874
Reid Ashwill <reidashwill@gmail.com>
2020-05-15 09:02:55 -0700

    add initialize, self.all, self.clear, self.find, save, and == methods to Definition class.  tests to follow

---
+++ lib/definition.rb
@@ -0,0 +1,35 @@
+ class Definition
+  attr_reader :id,
+  attr_accessor :def, :word_id
+  
+  @@definitions = {}
+  @@total_rows = 0
+
+  def initialize(attributes)
+    @def = attributes.fetch(:def)
+    @id = attributes.fetch(:id) || @@total_rows +=1
+    @word_id = attributes.fetch(:word_id)
+  end
+
+  def == (definition_to_compare)
+    (self.def() == definition_to_compare.def()) && (self.word_id() == definition_to_compare.word_id())
+  end
+
+  def self.all
+    @@definitions.values()
+  end
+  
+  def self.clear
+    @@definitions = {}
+    @@total_rows = 0
+  end
+
+  def self.find(id)
+    @@definitions[id]
+  end
+
+  def save
+    @@definitions[self.id] = Definition.new({ :def => self.def, :id => self.id, :word_id => self.word_id})
+  end
+
+end  
