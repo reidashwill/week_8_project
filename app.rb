18f33f71919ecaf5fbd35cf0fbabe7d37187a19e
Reid Ashwill <reidashwill@gmail.com>
2020-05-15 10:23:10 -0700

    create app.rb, layout.erb, and words.erb.  Add basic structure to layout.erb, and the home routes in app.rb

---
+++ app.rb
@@ -0,0 +1,15 @@
+require('sinatra')
+require('sinatra/reloader')
+require('./lib/word')
+require('./lib/definition')
+require('pry')
+also_reload('lib/**/*.rb')
+
+get('/') do
+  @words = Word.all
+  erb(:words)
+end
+
+get('/words') do
+  redirect to ('/')
+end
